79d198e0254b19ecf407f67f5d60fa120e3dc3af
benric8 <benric.366@gmail.com>
2024-08-13 15:06:30 -0500

    add validation of Ruc

--- consultaServiciosPide-ws-domain/src/main/java/pe/gob/pj/cspide/domain/utils/ProjectConstants.java
+++ consultaServiciosPide-ws-domain/src/main/java/pe/gob/pj/cspide/domain/utils/ProjectConstants.java
@@ -19,7 +19,7 @@ public class ProjectConstants {
 	
 	public class Aplicativo {
 		public static final String NOMBRE = "consultaServicioPide-ws";
-		public static final String VERSION = "1.2.0";
+		public static final String VERSION = "1.3.0";
 	}
 	
 	public class Auditoria {
--- consultaServiciosPide-ws-infraestructure/src/main/java/pe/gob/pj/cspide/infraestructure/rest/adapter/ConsultarSunatController.java
+++ consultaServiciosPide-ws-infraestructure/src/main/java/pe/gob/pj/cspide/infraestructure/rest/adapter/ConsultarSunatController.java
@@ -3,12 +3,17 @@ package pe.gob.pj.cspide.infraestructure.rest.adapter;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +36,7 @@ import pe.gob.pj.cspide.infraestructure.client.response.sunat.SunatRepresentates
 import pe.gob.pj.cspide.infraestructure.client.services.SunatClient;
 import pe.gob.pj.cspide.infraestructure.rest.response.GlobalResponse;
 
+@Validated
 @Slf4j
 @RestController
 @RequestMapping(value="sunat"  ,produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -48,7 +54,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="datos-principales")
 	public ResponseEntity<GlobalResponse> obtenerDatosPrincipales(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -73,7 +82,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="datos-secundarios")
 	public ResponseEntity<GlobalResponse> obtenerDatosSecundarios(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -98,7 +110,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="datos-t1144")
 	public ResponseEntity<GlobalResponse> obtenerDatost1144(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -123,7 +138,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="datos-t362")
 	public ResponseEntity<GlobalResponse> obtenerDatost362(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -148,7 +166,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="domicilio-legal")
 	public ResponseEntity<GlobalResponse> obtenerDomicilioLegal(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -173,7 +194,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="establecimietos-anexos")
 	public ResponseEntity<GlobalResponse> obtenerEstablecimientosAnexos(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -198,7 +222,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="establecimietos-anexos/t1150")
 	public ResponseEntity<GlobalResponse> obtenerEstAnexost1150(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
@@ -223,7 +250,10 @@ public class ConsultarSunatController implements Serializable{
 	@GetMapping(value="representantes-legales")
 	public ResponseEntity<GlobalResponse> obtenerRepresentantesLegales(@RequestAttribute(name=ProjectConstants.AUD_CUO) String cuo,
 			@RequestParam(name = "formatoRespuesta", defaultValue = "json", required = false) String formatoRespuesta,
-			@RequestParam(name = "numruc") String numeroRuc){
+			@Pattern(regexp = ProjectConstants.Pattern.NUMBER, message = "El parámetro numruc no tiene formato valido.")
+            @NotBlank(message = "El parámetro numruc no puede tener un valor vacio.")
+            @Length(min = 11, max = 11, message = "El parámetro numruc tiene un tamaño no valido [min=11,max=11].")
+            @RequestParam(name = "numruc") String numeroRuc){
 		GlobalResponse res =  new GlobalResponse();
 		res.setCodigoOperacion(cuo);
 		try {
